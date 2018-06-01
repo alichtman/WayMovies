ed3caadb56fabeba7a77eb3c0db7c6ee1322c939
Aaron Lichtman <alichtman@wayfair.com>
2018-06-01 15:56:04 -0400

    Initial Commit

---
+++ WayMovies2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WayMovies2
+//
+//  Created by Aaron Lichtman on 6/1/18.
+//  Copyright © 2018 Aaron Lichtman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
