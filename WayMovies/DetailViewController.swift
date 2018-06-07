963331358c23139bb3764cea64749b10fb42e746
Aaron Lichtman <alichtman@wayfair.com>
2018-06-07 17:09:37 -0400

    idk updates

--- WayMovies/DetailViewController.swift
+++ WayMovies/DetailViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    // TODO: Swipe to dismiss detail.
+    // https://gist.githubusercontent.com/gyubokbaik/d2ec06fed597759c56aa62c5ff71e9a0/raw/2c9fde04404914cb6e1f5b07db1f9f66e82fe558/panGestureRecognizerHandler.txt
+    
     var movieDetail: MovieDetails
+    var initialTouchPoint: CGPoint = CGPoint(x: 0,y: 0)
+    
     
     init(movieDetail: MovieDetails) {
         self.movieDetail = movieDetail
@@ -20,6 +25,27 @@ class DetailViewController: UIViewController {
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    @IBAction func panGestureRecognizerHandler(_ sender: UIPanGestureRecognizer) {
+        let touchPoint = sender.location(in: self.view?.window)
+        
+        if sender.state == UIGestureRecognizerState.began {
+            initialTouchPoint = touchPoint
+        } else if sender.state == UIGestureRecognizerState.changed {
+            if touchPoint.y - initialTouchPoint.y > 0 {
+                self.view.frame = CGRect(x: 0, y: touchPoint.y - initialTouchPoint.y, width: self.view.frame.size.width, height: self.view.frame.size.height)
+            }
+        } else if sender.state == UIGestureRecognizerState.ended || sender.state == UIGestureRecognizerState.cancelled {
+            if touchPoint.y - initialTouchPoint.y > 100 {
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                UIView.animate(withDuration: 0.3, animations: {
+                    self.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+                })
+            }
+        }
     }
     
     override func viewDidLoad() {
